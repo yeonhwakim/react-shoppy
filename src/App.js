64513a2af1191d92f8c7a49e85209adc9abfe86d
yeonhwa <yeonhwa6717@gmail.com>
2023-06-15 21:44:29 +0900

    feat: Set root router.
    Create a new file called 'Home.jsx' and render the simple string 'Home'.
    Create a new file called 'Error.jsx' and render the notificaiton text.
    Import the 'createBrowserRouter' from the 'react-router-dom' and pass an array of 'Route' object as an argument.
    Add the 'errorElement' property to the array of 'Route' objects.
    Add the nested 'children' property to the array of 'Route' objects.
    Render the component based on the routes using the 'Outlet' component from 'react-router-dom' in the 'App' component.
    In the first nested children of 'Routes' object, set the 'index' property to true and render the 'Home' component to handel the index router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">SHOPPY</div>;
+  return (
+    <div className="App">
+      <div>HEADER</div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+import Error from "./components/Error";
+import reportWebVitals from "./reportWebVitals";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
