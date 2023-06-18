23a5e70dc80cb308a0c0ef2be25c76f748336c13
yeonhwa <yeonhwa6717@gmail.com>
2023-06-18 22:39:43 +0900

    chore: Add @tanstack/react-query dependencies.
    Add the necessary dependencies for using @tanstack/react-query.
    In the 'App' component, import the 'QueryClient' and 'QueryClientProvider' from '@tanstack/react-query'.
    Wrap the 'Header' and 'Outlet' components with the 'QueryClientProvider' component, providing the 'queryClient' instance as the 'client' prop.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./App.css";
 import Header from "./components/Header";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <Outlet />
+      </QueryClientProvider>
     </div>
   );
 }
