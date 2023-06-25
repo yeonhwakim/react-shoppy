bb9262fe8f1c20960bfe9bc65280d70ee5f181a3
yeonhwa <yeonhwa6717@gmail.com>
2023-06-25 17:20:11 +0900

    markup: Markup 'Home' and 'Products' components.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Products from "./Products";
 
 function Home(props) {
-  return <div>HOME</div>;
+  return (
+    <div>
+      <div>
+        <p>OPENING SALE!</p>
+        <p>2023.06.01~2023.06.30</p>
+      </div>
+      <Products />
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
 function Products(props) {
-  return <div>Products</div>;
+  return (
+    <ul>
+      <li>
+        <div>이미지</div>
+        <div>
+          <span>타이틀</span>
+          <span>가격</span>
+        </div>
+        <div>
+          <span>카테고리</span>
+        </div>
+      </li>
+      <li>
+        <div>이미지</div>
+        <div>
+          <span>타이틀</span>
+          <span>가격</span>
+        </div>
+        <div>
+          <span>카테고리</span>
+        </div>
+      </li>
+      <li>
+        <div>이미지</div>
+        <div>
+          <span>타이틀</span>
+          <span>가격</span>
+        </div>
+        <div>
+          <span>카테고리</span>
+        </div>
+      </li>
+      <li>
+        <div>이미지</div>
+        <div>
+          <span>타이틀</span>
+          <span>가격</span>
+        </div>
+        <div>
+          <span>카테고리</span>
+        </div>
+      </li>
+      <li>
+        <div>이미지</div>
+        <div>
+          <span>타이틀</span>
+          <span>가격</span>
+        </div>
+        <div>
+          <span>카테고리</span>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default Products;
