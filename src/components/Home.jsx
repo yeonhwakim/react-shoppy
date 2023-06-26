4eabd45eeb5fca82ecee7ef24973aa4937b998fa
yeonhwa <yeonhwa6717@gmail.com>
2023-06-26 22:10:13 +0900

    style: Style 'Home' and 'Products' component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import Products from "./Products";
 function Home(props) {
   return (
     <div>
-      <div>
-        <p>OPENING SALE!</p>
-        <p>2023.06.01~2023.06.30</p>
+      <div className="w-full bg-black px-2 py-10 text-white flex flex-col items-center">
+        <p className="text-white font-bold text-2xl mb-2">OPENING SALE!</p>
+        <p className="text-white">2023.06.01~2023.06.30</p>
       </div>
       <Products />
     </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -11,17 +11,24 @@ function Products() {
   }, []);
 
   return (
-    <ul>
+    <ul className="p-4 grid grid-cols-4 gap-4">
       {products &&
         products.map(({ id, image, name, price, category }) => (
-          <li key={id}>
-            <img src={image} alt={`${name} 이미지`} />
-            <div>
-              <span>{name}</span>
-              <span>{price}</span>
-            </div>
-            <div>
-              <span>{category}</span>
+          <li
+            className="rounded-md flex flex-col items-center shadow-xl overflow-hidden"
+            key={id}
+          >
+            <img className="w-50" src={image} alt={`${name} 이미지`} />
+            <div className="w-full p-2">
+              <div className="flex flex-row justify-between">
+                <span className="text-base font-bold">{name}</span>
+                <span className="text-base font-bold">₩{price}</span>
+              </div>
+              <div>
+                <span className="text-sm font-semibold text-gray-500">
+                  {category}
+                </span>
+              </div>
             </div>
           </li>
         ))}
