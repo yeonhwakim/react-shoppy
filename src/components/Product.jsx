b7a739eee057504480ca452801413ca8cdbf99f9
yeonhwa <yeonhwa6717@gmail.com>
2023-06-27 22:27:34 +0900

    feat: Render specific product information.
    Retrieve the 'id' prameter from the route using the 'usePrams' hook.
    Pass the 'id' parameter to the 'getProduct' function as argument to retireve the specific product information.
    Render the information of the 'product'.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function Product(props) {
-  return <div>Product</div>;
+import { getProduct } from "../api/firebase";
+
+function Product() {
+  const { id } = useParams();
+
+  const [product, setProduct] = useState({});
+  const { category, description, image, name, option, price } = product;
+
+  useEffect(() => {
+    getProduct({ id }).then((product) => {
+      setProduct(product);
+    });
+  }, []);
+
+  return (
+    product && (
+      <div>
+        <div>{`>${category}`}</div>
+        <div>
+          <img src={image} alt={`${name} 이미지`} />
+          <div>
+            <div>
+              <span>{name}</span>
+              <span>₩{price}</span>
+            </div>
+            <p>{description}</p>
+            <div>
+              <span>옵션 :</span>
+              <select name="" id="">
+                {option &&
+                  option.split(",").map((item) => (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <button>장바구니에 추가</button>
+          </div>
+        </div>
+      </div>
+    )
+  );
 }
 
 export default Product;
