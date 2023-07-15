8c1bbda114265d2751c5ba0ebb25c4cdabfae682
yeonhwa <yeonhwa6717@gmail.com>
2023-07-15 10:08:59 +0900

    refactor: Modify select element to input element.
    Modify the select element to an input element for the 'options' state in the 'NewProduct' comopnent.

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -7,23 +7,14 @@ import { addProduct } from "../api/firebase";
 
 import File from "../components/File";
 import Input from "../components/Input";
-import Select from "../components/Select";
 import Button from "../components/Button";
-
-const options = [
-  { value: "1", option: "xs,s,m,l,xl" },
-  { value: "2", option: "xxs,xs,s,m,l,xl,xx" },
-  { value: "3", option: "6,38,40,42" },
-  { value: "4", option: "상의,하의,탈의" },
-  { value: "5", option: "상의,하의" },
-  { value: "6", option: "r,t,y" },
-];
 
 const inputList = [
   { name: "name", type: "text", placeholder: "제품명" },
   { name: "price", type: "number", placeholder: "가격" },
   { name: "category", type: "text", placeholder: "카테고리" },
   { name: "description", type: "text", placeholder: "제품설명" },
+  { name: "options", type: "text", placeholder: "옵션들 (콤마(,)로 구분)" },
 ];
 
 function NewProduct() {
@@ -36,7 +27,7 @@ function NewProduct() {
     price: "",
     category: "",
     description: "",
-    option: "",
+    options: "",
   });
 
   const { image } = product;
@@ -45,10 +36,7 @@ function NewProduct() {
     const { name, value } = e.target;
     setProduct((product) => ({
       ...product,
-      [name]:
-        name === "option"
-          ? options.filter(({ value: id }) => id === value)[0].option
-          : value,
+      [name]: name === "options" ? value.split(",") : value,
     }));
   };
 
@@ -101,7 +89,6 @@ function NewProduct() {
             handleChangeProduct={handleChangeProduct}
           />
         ))}
-        <Select options={options} handleChangeProduct={handleChangeProduct} />
         <Button title={"제품 등록하기"} />
       </form>
     </div>
