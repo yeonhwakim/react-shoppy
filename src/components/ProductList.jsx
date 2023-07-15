09f4079f0aa07225e2daff9246dc6209fd0be333
yeonhwa <yeonhwa6717@gmail.com>
2023-07-15 21:51:43 +0900

    style: Modify style css

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,7 +4,7 @@ import ProductItem from "../components/ProductItem";
 
 function ProductList({ products, handleClickProduct }) {
   return (
-    <ul className="p-4 grid grid-cols-4 gap-4">
+    <ul className="p-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {products &&
         products.map((product) => (
           <ProductItem
